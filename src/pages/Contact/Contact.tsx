6cf3dddcaa428821165fb395c5de55077b4bff03
jottiana <11.paradise.avenue@gmail.com>
2025-03-30 01:34:20 +0100

    feat(Contact): create contact page

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,14 +1,15 @@
+import "./Contact.css"
 import { useGlobalContext } from "../../context/GlobalContext";
 import { translations } from "../../data/translations";
 
 function Contact() {
 	const { language } = useGlobalContext();
-	const t = translations.contact;
+	const t = translations[language].contact;
 
 	return (
-		<div className="page">
-			<h1>{t.title[language as "fr" | "en"]}</h1>
-			<p>{t.content[language as "fr" | "en"]}</p>
+		<div className="contact-page">
+			<h1>{t.title}</h1>
+			<p>{t.content}</p>
 		</div>
 	);
 }

3d08f0649b2f06053d2c7d813364ff5c473ab7df
jottiana <11.paradise.avenue@gmail.com>
2025-05-09 13:59:58 +0200

    feat(Footer): update footer content with logo, links and social medias

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,39 +1,157 @@
 import "./Footer.css";
+import colibriLogo from "/assets/colibri-logo.png";
+import facebookLogo from "/assets/facebook-icon.svg";
+import helloassoLogo from "/assets/helloasso-icon.svg";
+import instagramLogo from "/assets/instagram-icon.svg";
+import { useGlobalContext } from "../../context/GlobalContext";
+import { translations } from "../../data/translations";
 
 function Footer() {
-  return (
-    <footer className="footer">
-      <div className="footer-container">
-        <p className="footer-text">© {new Date().getFullYear()} Le Colibri Indien</p>
-        <div className="footer-links">
-          <a
-            href="https://www.facebook.com/profile.php?id=61574921468907"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="footer-link"
-          >
-            Facebook
-            </a>
-          <a
-            href="https://www.helloasso.com/associations/association-le-colibri-indien"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="footer-link"
-          >
-            HelloAsso
-          </a>
-          <a
-            href="https://www.instagram.com/le_colibri_indien/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="footer-link"
-          >
-            Instagram
-          </a>
-        </div>
-      </div>
-    </footer>
-  );
+	const { language } = useGlobalContext();
+	const t = translations[language].footer;
+
+	return (
+		<footer className="footer">
+			<div className="footer-top">
+				<div className="footer-logo-wrapper">
+					<img src={colibriLogo} alt="Logo Colibri" className="footer-logo" />
+				</div>
+			</div>
+
+			<div className="footer-columns">
+				<div className="footer-column">
+					<h3>{t.aboutTitle}</h3>
+					<ul>
+						<li>
+							<a href="/aboutus">{t.aboutLinks.discover}</a>
+						</li>
+						<li>
+							<a href="/actions">{t.aboutLinks.actions}</a>
+						</li>
+						<li>
+							<a href="/contact">{t.aboutLinks.contact}</a>
+						</li>
+						<li>
+							<a href="/event">{t.aboutLinks.news}</a>
+						</li>
+						<li>
+							<a href="/legalnotice">{t.aboutLinks.legal}</a>
+						</li>
+					</ul>
+				</div>
+
+				<div className="footer-column">
+					<h3>{t.actTitle}</h3>
+					<ul>
+						<li>
+							<a
+								href="https://www.helloasso.com/associations/association-le-colibri-indien/formulaires/1"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								{t.actLinks.donate}
+							</a>
+						</li>
+						<li>
+							<a
+								href="https://www.helloasso.com/associations/association-le-colibri-indien/adhesions/adhesion-le-colibri-indien"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								{t.actLinks.member}
+							</a>
+						</li>
+						<li>
+							<a
+								href="https://www.helloasso.com/associations/association-le-colibri-indien/evenements/billetterie-gala-de-charite"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								{t.actLinks.gala}
+							</a>
+						</li>
+					</ul>
+				</div>
+
+				<div className="footer-column">
+					<h3>{t.socialTitle}</h3>
+					<ul>
+						<li>
+							<a
+								href="https://www.facebook.com/le.colibri.indien/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								Facebook
+							</a>
+						</li>
+						<li>
+							<a
+								href="https://www.helloasso.com/associations/association-le-colibri-indien"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								HelloAsso
+							</a>
+						</li>
+						<li>
+							<a
+								href="https://www.instagram.com/le_colibri_indien/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								Instagram
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+
+			<div className="footer-bottom">
+				<p>© {new Date().getFullYear()} Le Colibri Indien</p>
+
+				<div className="footer-social-icons">
+					<a
+						href="https://www.facebook.com/le.colibri.indien/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<div className="social-icon-wrapper">
+							<img src={facebookLogo} alt="Facebook" />
+						</div>
+					</a>
+					<a
+						href="https://www.helloasso.com/associations/association-le-colibri-indien"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<div className="social-icon-wrapper">
+							<img src={helloassoLogo} alt="HelloAsso" />
+						</div>
+					</a>
+					<a
+						href="https://www.instagram.com/le_colibri_indien/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<div className="social-icon-wrapper">
+							<img src={instagramLogo} alt="Instagram" />
+						</div>
+					</a>
+				</div>
+
+				<p>
+					<a
+						href="https://github.com/jottiana"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						{t.siteBy}
+					</a>
+				</p>
+			</div>
+		</footer>
+	);
 }
 
 export default Footer;
